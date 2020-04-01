011c26765df2b3d7970f2e522512396243ccf25b
ammar903@gmail.com <ammar903@gmail.com>
2020-04-02 00:28:57 +0100

    cleaning up

--- Ammar/plot_graph.m
+++ Ammar/plot_graph.m
@@ -1,9 +1,10 @@
 load('30min2cm.mat');
 
-semilogy(strain, abs(resistance),'k');
+semilogy(strain, abs(1./resistance),'k');
 ax = gca;
 ax.XLim = [-0.1,0.8];
 
 xlabel('Strain (\Deltacm/cm)');
-ylabel('Resistance (\Omega/cm)');
+ylabel('Conductance (S/cm)');
 set(gca,'Fontname','CMU bright','Fontsize',14);
+print(gcf,'strain_conductance.png','-dpng','-r900');
